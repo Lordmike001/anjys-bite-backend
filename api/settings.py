d9df48ac4e217f833ee0fa1f2461c754a43b2d05
Hade-Mide <adenusisamuell@gmail.com>
2026-09-08 16:06:21 +0100

    user app created

--- api/settings.py
+++ api/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # external installed libraries
+    'drf_yasg',
+
+    # installed apps
+    'user',
 ]
 
 MIDDLEWARE = [
